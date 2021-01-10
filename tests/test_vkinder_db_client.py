c6875945e42ab5411c9c743d21c777c3d3497483
Yuri <Yuribtr@users.noreply.github.com>
2021-01-10 20:31:28 +0300

    Added test of client save to DB

---
+++ tests/test_vkinder_db_client.py
@@ -0,0 +1,28 @@
+import unittest
+from unittest import mock
+from tests.mock_server import get_free_port, start_mock_server
+from сlasses.vk_api_classes import VKinderClient
+from сlasses.vk_api_client import VkApiClient
+from сlasses.vkinder_db_client import VKinderDb
+
+
+class TestVKinderDb(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.db = VKinderDb('test', 'test', 'test', debug_mode=True)
+        cls.mock_server_port = get_free_port()
+        start_mock_server(cls.mock_server_port)
+
+    def test_db_init(self):
+        assert self.db.is_initialized
+        mock_users_url = 'http://localhost:{port}/'.format(port=self.mock_server_port)
+        with mock.patch('сlasses.vk_api_client.VkApiClient.API_BASE_URL', new_callable=mock.PropertyMock) as mock_f:
+            mock_f.return_value = mock_users_url
+            self.api = VkApiClient(token='', app_id='', user_id='1', debug_mode=True)
+            assert self.api.is_initialized
+            users = self.api.get_users('1')
+            assert len(users) == 1
+            user = self.api.get_users('1')[0]
+            client = VKinderClient(user)
+            self.db.save_client(client)
